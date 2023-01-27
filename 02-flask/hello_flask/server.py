5e19f0915f99639e02ef6aeeda7cb10a04affec6
Henry Chen <henrychen14505@gmail.com>
2023-01-27 15:56:37 -0800

    html_table assignment completed

--- 02-flask/hello_flask/server.py
+++ 02-flask/hello_flask/server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def hello_world():
     # Instead of returning a string, 
     # we'll return the result of the render_template method, passing in the name of our HTML file
-    return render_template('index.html', phrase="hello", times=5)         
+    return render_template('index.html', phrase="hello", times=5)
 
 @app.route('/success')
 def success():
@@ -22,8 +22,16 @@ def show_user_profile(username, id):
     print(id)
     return f"username: {username}, id: {id}"
 
-def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+        {'name' : 'Michael', 'age' : 35},
+        {'name' : 'John', 'age' : 30 },
+        {'name' : 'Mark', 'age' : 25},
+        {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template('lists.html', random_numbers = [3,1,5], students = student_info)
 
-if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-    app.run(host='localhost', port=5001)    # Run the app in debug mode.      
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module
+    app.run(debug=True, host='localhost', port=5003)    # Run the app in debug mode.
