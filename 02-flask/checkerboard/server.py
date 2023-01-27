ea580e4b00a010f784a719138e1b00b57d6833fd
Henry Chen <henrychen14505@gmail.com>
2023-01-27 14:51:34 -0800

    checkerboard assignment_completed

--- 02-flask/checkerboard/server.py
+++ 02-flask/checkerboard/server.py
@@ -5,24 +5,29 @@ app = Flask(__name__)
 def root():
     return render_template('index.html')
 
+@app.route('/<int:num_rows>')
+def gen_rows(num_rows):
+    num_rows_half = int(num_rows/2)
+    return render_template('index.html', num_rows_half=num_rows_half, num_rows=num_rows)
+
+@app.route('/<int:num_rows>/<int:num_cols>')
+def gen_rows_cols(num_rows, num_cols):
+    num_rows_half = int(num_rows/2)
+    num_cols_half = int(num_cols/2)
+    return render_template('index.html', num_rows_half=num_rows_half, num_rows=num_rows, num_cols_half=num_cols_half, num_cols=num_cols)
+
+@app.route('/<int:num_rows>/<int:num_cols>/<string:color_1>/<string:color_2>')
+def gen_rows_cols_colors(num_rows, num_cols, color_1, color_2):
+    num_rows_half = int(num_rows/2)
+    num_cols_half = int(num_cols/2)
+    return render_template('index.html', num_rows_half=num_rows_half, num_rows=num_rows, num_cols_half=num_cols_half, num_cols=num_cols, color_1=color_1, color_2=color_2)
+
 if __name__=="__main__": 
-    app.run(host='localhost', port=5005)         
+    app.run(debug=True, host='localhost', port=5005)
 
-# @app.route('/success')
-# def success():
-#     return "success"
 
-# @app.route('/hello/<name>')
-# def hello(name):
-#     print(name)
-#     return "Hello, " + name
 
-# @app.route('/users/<username>/<id>')
-# def show_user_profile(username, id):
-#     print(username)
-#     print(id)
-#     return f"username: {username}, id: {id}"
 
-# def hello_world():
-#     return 'Hello World!'  # Return the string 'Hello World!' as a response
+
+
 
