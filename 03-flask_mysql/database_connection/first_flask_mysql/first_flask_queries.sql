db480d1906a73e62906fa5cc3a2ea4a9e485e898
Henry Chen <henrychen14505@gmail.com>
2023-01-31 16:38:41 -0800

    connecting_to_a_database assignment working on

---
+++ 03-flask_mysql/database_connection/first_flask_mysql/first_flask_queries.sql
@@ -0,0 +1,13 @@
+insert into friends (first_name, last_name, created_at, updated_at)
+values("Ellie", "Chen", "student", now(), now());
+
+insert into friends (first_name, last_name, occupation, created_at, updated_at)
+values("Charlene", "Chen", "pharmacist", now(), now());
+
+insert into friends (first_name, last_name, occupation, created_at, updated_at)
+values("David", "Chen", "carpenter", now(), now());
+
+insert into friends (first_name, last_name, occupation, created_at, updated_at)
+values("Cindy", "Chen", "clerk", now(), now());
+
+select * from friends;
