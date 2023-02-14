0344bf097eed8d1df03aa13b78d40138d92d0cac
Henry Chen <henrychen14505@gmail.com>
2023-02-13 21:54:45 -0800

    recipes assignment completed

--- 01-mysql/mysql_queries/books_queries.sql
+++ 01-mysql/mysql_queries/books_queries.sql
@@ -11,7 +11,6 @@ JOIN authors ON authors.id = favorites.author_id
 WHERE favorites.book_id = 7;
 
 SELECT * FROM books
-JOIN favorites ON books.id = favorites.book_id
-JOIN authors ON authors.id = favorites.author_id
-WHERE favorites.author_id != 7;
+WHERE books.id NOT IN ( SELECT book_id FROM favorites WHERE author_id = 11);
 				
+SELECT * FROM authors;
---
+++ 01-mysql/mysql_queries/recipes_queries.sql
@@ -0,0 +1,22 @@
+INSERT INTO recipes (name, description, instructions, date, under_30_mins, users_id)
+                    VALUES ("Brownies", "Very delicious", "Bake for 10 mins", "2023-02-13", "Yes", "1");
+SELECT * from recipes
+JOIN users ON users.id = recipes.users_id;
+
+SELECT * FROM users
+JOIN recipes ON recipes.users_id = users.id;
+
+SELECT * FROM recipes
+JOIN users ON users.id = recipes.users_id;
+
+SELECT * FROM recipes
+JOIN users ON users.id = recipes.users_id
+WHERE recipes.id = 5;
+
+SELECT * FROM users
+JOIN recipes ON users.id = recipes.users_id
+WHERE recipes.id = 1;
+
+SELECT * FROM users;
+
+		
