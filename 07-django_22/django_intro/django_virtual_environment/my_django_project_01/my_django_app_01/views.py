03d5aa30c0697bac4557fc5406af2c0b3c2fa986
Henry Chen <henrychen14505@gmail.com>
2023-02-20 14:35:10 -0800

    time_display_assignment_completed

--- 07-django_22/django_intro/django_virtual_environment/my_django_project_01/my_django_app_01/views.py
+++ 07-django_22/django_intro/django_virtual_environment/my_django_project_01/my_django_app_01/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render, HttpResponse
+# def index(request):
+#     # return HttpResponse("this is the equivalent of @app.route('/')!")
+#     return HttpResponse("response from index method from root route, localhost:8000!")
+
 def index(request):
-    # return HttpResponse("this is the equivalent of @app.route('/')!")
-    return HttpResponse("response from index method from root route, localhost:8000!")
+    context = {
+        "name": "Noelle",
+        "favorite_color": "turquoise",
+        "pets": ["Bruce", "Fitz", "Georgie"],
+    }
+    return render(request, "index.html", context)
+
+def first_name(request, first_name):
+    context = {
+        "first_name": first_name,
+        "name": "Noelle",
+        "favorite_color": "turquoise",
+        "pets": ["Bruce", "Fitz", "Georgie"],
+    }
+    return render(request, "index2.html", context)
