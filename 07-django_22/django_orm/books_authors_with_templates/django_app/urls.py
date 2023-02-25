9416e23bc9c6268494d8e965ea41a3e96c99f1d9
Henry Chen <henrychen14505@gmail.com>
2023-02-25 12:18:09 -0800

    books_authors_with_templates assignment completed

--- 07-django_22/django_orm/books_authors_with_templates/django_app/urls.py
+++ 07-django_22/django_orm/books_authors_with_templates/django_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("books/<int:book_id>", views.book_page),
     path("authors", views.authors_page),
     path("add_author_form", views.add_author_form),
-    path("authors/<int:author_id>", views.author_page)
+    path("authors/<int:author_id>", views.author_page),
+    path("add_author_to_book_form/<int:book_id>", views.add_author_to_book_form),
+    path("add_book_to_author_form/<int:author_id>", views.add_book_to_author_form)
     ]
--- 07-django_22/django_orm/books_authors_with_templates/django_app/views.py
+++ 07-django_22/django_orm/books_authors_with_templates/django_app/views.py
@@ -20,9 +20,16 @@ def add_book_form(request):
 def book_page(request, book_id):
     context = {
         "this_book": Book.objects.get(id=book_id),
-        "all_authors": Author.objects.all()
+        "all_authors": Author.objects.all(),
+        "all_authors_for_this_book": Book.objects.get(id=book_id).authors.all()
     }
     return render(request, "book_view.html", context)
+
+def add_book_to_author_form(request, author_id):
+    this_author = Author.objects.get(id=author_id)
+    this_book = Book.objects.get(id=request.POST["book_id"])
+    this_book.authors.add(this_author)
+    return redirect(f'/authors/{ author_id }')
 
 ##### Authors #####
 
@@ -34,7 +41,9 @@ def authors_page(request):
 
 def author_page(request, author_id):
     context = {
-        "this_author": Author.objects.get(id=author_id)
+        "this_author": Author.objects.get(id=author_id),
+        "all_books": Book.objects.all(),
+        "all_books_for_this_author": Author.objects.get(id=author_id).books.all()
     }
     return render(request, "author_view.html", context)
 
@@ -46,3 +55,9 @@ def add_author_form(request):
         # dojo = Dojo.objects.get(id = request.POST["dojo_id"]))
     )
     return redirect("/authors")
+
+def add_author_to_book_form(request, book_id):
+    this_book = Book.objects.get(id=book_id)
+    this_author = Author.objects.get(id=request.POST["author_id"])
+    this_author.books.add(this_book)
+    return redirect(f'/books/{ book_id }')
